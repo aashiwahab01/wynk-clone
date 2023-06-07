92856d5c49ccf1d6b652baa64c74b0b5805ad983
aashiwahab01 <118872356+aashiwahab01@users.noreply.github.com>
2023-06-07 13:06:14 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,15 @@
+var mySong =document.getElementById("mySong");
+    var icon =document.getElementById("icon");
+
+   icon.onclick=function(){
+      if(mySong. paused){
+          mySong.play();
+          icon.src="pause.png";
+          
+      }
+      else{
+          mySong.pause();
+          icon.src="play.png";
+      }
+   }
+   
